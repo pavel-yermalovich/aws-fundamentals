9310437164f4d793c4d4dad09fa5ea9c4ec2be98
pavel-yermalovich <x48ray@gmail.com>
2025-01-05 10:25:17 +0100

    Added an empty lambda function

---
+++ SimpleLambda/src/SimpleLambda/Function.cs
@@ -0,0 +1,28 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambda
+{
+	public class Function
+	{
+
+		/// <summary>
+		/// A simple function that takes a string and does a ToUpper
+		/// </summary>
+		/// <param name="input">The event for the Lambda function handler to process.</param>
+		/// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+		/// <returns></returns>
+		public string FunctionHandler(Hello request, ILambdaContext context)
+		{
+			context.Logger.LogInformation($"Hello from {request.World}!");
+			return request.World;
+		}
+	}
+
+	public class Hello
+	{
+		public string World { get; set; } = default!;
+	}
+}
